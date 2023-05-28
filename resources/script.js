0345eb1e8db130ac01eb9ef2cf1a5904fc6bb8df
CarolinaSFreitas <carolsoaresfreitas@hotmail.com>
2023-05-28 11:49:06 -0300

    menu-burguer-init

---
+++ resources/script.js
@@ -0,0 +1,10 @@
+function openMenu() {
+    var menu = document.getElementsByClassName('.nav_links2');
+
+    if (menu.display === "block") {
+        menu.style.display = "none";
+
+    } else {
+        menu.style.display = "block";
+    }
+}
