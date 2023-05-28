39d7dcbe3fba8a9d88b1548b9fd5914b88161c55
CarolinaSFreitas <carolsoaresfreitas@hotmail.com>
2023-05-28 14:45:05 -0300

    btn-top

--- resources/script.js
+++ resources/script.js
@@ -8,3 +8,24 @@ function openMenu() {
         menu.style.display = "block";
     }
 }
+
+// botao de voltar pro topo
+window.onscroll = function () {
+    showButton();
+};
+
+function showButton() {
+    var btn = document.getElementById("btnTopo");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btn.style.display = "block";
+    }
+    else {
+        btn.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+// 
